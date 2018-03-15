0c2770ca0371a208e6fa2b1a0395bcd63c7f4f63
SnakeMrLiu <liuds01261008@163.com>
2018-03-15 14:20:32 +0800

    Signed-off-by: SnakeMrLiu <liuds01261008@163.com>

---
+++ layuiboot/src/main/java/com/lbw/LayuibootApplication.java
@@ -0,0 +1,26 @@
+package com.lbw;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@MapperScan(value = "com.lbw.mapper")
+public class LayuibootApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayuibootApplication.class, args);
+	}
+
+	/**
+	 * 支持jsp需要重写此方法
+	 * @param builder
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
+}
